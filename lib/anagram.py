f2243c490996270784aff21b4d506fb97a3570b5
Enock Korir <enockorir123@gmail.com>
2025-05-22 17:46:35 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, candidates):
+        sorted_word = sorted(self.word)
+        return [candidate for candidate in candidates if sorted(candidate) == sorted_word]
+
+      
